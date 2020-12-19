8875ed8497284119d3c12764bf10aa415d136db8
Krisztián Ormós <ormos.krisztian.10@gmail.com>
2020-12-19 20:40:31 +0100

    add styling and formating to pages

--- RFT_Webshop/RFT_Webshop/server.js
+++ RFT_Webshop/RFT_Webshop/server.js
@@ -10,6 +10,7 @@ const passport = require('passport')
 const flash = require('express-flash')
 const session = require('express-session')
 const methodOverride = require('method-override')
+const dotenv = require('dotenv').config()
 
 const initPP = require('./passport-config')
 initPP(
